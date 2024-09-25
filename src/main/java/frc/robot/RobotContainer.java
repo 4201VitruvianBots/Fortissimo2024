22a98688149b9272decaece850ea58dd0c491962
Nathan S <nathanael7639@gmail.com>
2024-09-24 19:34:40 -0700

    Added things to the robot container to start the swerve.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -9,6 +9,13 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandPS4Controller;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.constants.USB;
+import frc.robot.constants.SWERVE.DRIVE;
+import frc.robot.constants.*; 
+import frc.robot.commands.characterization.SwerveDriveDynamic;
+import frc.robot.commands.characterization.SwerveDriveQuasistatic;
+import frc.robot.commands.characterization.SwerveTurnDynamic;
+import frc.robot.commands.characterization.SwerveTurnQuasistatic;
+import com.ctre.phoenix6.mechanisms.swerve.SwerveRequest;
 
 // import frc.robot.commands.autos.*;
 
@@ -31,12 +38,47 @@ public class RobotContainer {
       new CommandXboxController(USB.xBoxController);
   private final CommandPS4Controller testController = new CommandPS4Controller(USB.testController);
 
+  private final CommandSwerveDrivetrain m_swerveDrive =
+  new CommandSwerveDrivetrain(
+      SWERVE.DrivetrainConstants,
+      SWERVE.FrontLeftConstants,
+      SWERVE.FrontRightConstants,
+      SWERVE.BackLeftConstants,
+      SWERVE.BackRightConstants);
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
     initAutoChooser();
     configureBindings();
+    m_vision.registerSwerveDrive(m_swerveDrive);
+    m_controls.registerDriveTrain(m_swerveDrive);
+    m_swerveDrive.registerTelemetry(m_telemetry::telemeterize);
+    m_swerveDrive.registerVisionSubsystem(m_vision);
+
   }
 
+  private void initalizeSubsystems() {
+    if (RobotBase.isReal()) {
+      m_swerveDrive.setDefaultCommand(
+          m_swerveDrive.applyChassisSpeeds(
+              () ->
+                  new ChassisSpeeds(
+                      leftJoystick.getRawAxis(1) * DRIVE.kMaxSpeedMetersPerSecond,
+                      leftJoystick.getRawAxis(0) * DRIVE.kMaxSpeedMetersPerSecond,
+                      rightJoystick.getRawAxis(0) * DRIVE.kMaxRotationRadiansPerSecond)));
+    } else {
+      m_swerveDrive.setDefaultCommand(
+          m_swerveDrive.applyChassisSpeeds(
+              () ->
+                  new ChassisSpeeds(
+                      -m_testController.getRawAxis(0) * DRIVE.kMaxSpeedMetersPerSecond,
+                      m_testController.getRawAxis(1) * DRIVE.kMaxSpeedMetersPerSecond,
+                      -m_testController.getRawAxis(2) * DRIVE.kMaxRotationRadiansPerSecond)));
+
+      m_testController
+          .cross()
+          .whileTrue(new SetTrackingState(m_swerveDrive, TRACKING_STATE.SPEAKER));
+    }
+  }
   // Use this method to define your trigger->command mappings.
   private void configureBindings() {}
 
@@ -51,5 +93,9 @@ public class RobotContainer {
    */
   public Command getAutonomousCommand() {
     return null;
+  }
+  public void disabledInit(){
+    m_swerveDrive.applyRequest(SwerveRequest.ApplyChassisSpeeds::new);
+    m_swerveDrive.setTrackingState(TRACKING_STATE.NONE);
   }
 }
